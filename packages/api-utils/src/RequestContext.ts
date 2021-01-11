49eb3657e996c7c3dc5e6bf90fc65d0ef3f4a5bc
Danyil Moroz <danilmoroz1995@gmail.com>
2021-01-11 04:35:29 +0200

    @tsrt/api-utils - Added another version for async context using express-http-context

--- packages/api-utils/src/RequestContext.ts
+++ packages/api-utils/src/RequestContext.ts
@@ -1,7 +1,45 @@
-import { AsyncContext } from './AsyncContext';
+import { Request, Response, NextFunction, RequestHandler } from 'express';
+import * as contextStore from 'express-http-context';
 
-/* eslint-disable-next-line @typescript-eslint/no-empty-interface */
-export interface IRequestContext {}
-// declare global { export interface IRequestContext {} }
+import { combineMiddlewares } from './utils';
 
-export const requestContext = new AsyncContext<IRequestContext>();
+export class RequestContext<T extends GenericObject> {
+  public get use(): RequestHandler {
+    return combineMiddlewares(contextStore.middleware, this.bind);
+  }
+
+  public set(initialContext?: T): T {
+    if (!initialContext) return;
+
+    Object.keys(initialContext).forEach((key) => {
+      if (Object.prototype.hasOwnProperty.call(initialContext, key)) contextStore.set(key, initialContext[key]);
+    });
+
+    return this.get();
+  }
+
+  public get(): T;
+  public get<K extends keyof T>(key?: K): T[K];
+  public get<K extends keyof T>(key?: K): T | T[K] {
+    if (key) return contextStore.get(String(key));
+
+    const context = { ...contextStore.ns.active };
+    delete context.id;
+    delete context._ns_name;
+
+    return context;
+  }
+
+  /** Middleware to bind context to req & res */
+  private bind(req: Request, res: Response, next: NextFunction): void {
+    contextStore.ns.bindEmitter(req);
+    contextStore.ns.bindEmitter(res);
+    next();
+  }
+}
+
+/* eslint-disable-next-line */
+export interface IRequestContext { }
+
+/** Default requestContext to be used without instanciating new instance. */
+export const requestContext = new RequestContext<IRequestContext>();
